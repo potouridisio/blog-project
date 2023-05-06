4c20381d652b820f789037e2b5879ce4b5ef07d6
Ioannis Potouridis <potouridisio@gmail.com>
2023-05-06 17:01:00 +0300

    refactor: update the UI of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import { AiOutlineLike, AiFillLike } from 'react-icons/ai';
-import { BiComment } from 'react-icons/bi';
 
 import { getPosts, getSession, getUsers } from './api';
+import { getInitials, getInitialsColor } from './utils';
 
 // η App είναι ένα functional component
 // ένα functional component είναι μία συνάρτηση που επιστρέφει JSX
@@ -40,7 +40,9 @@ function App() {
 
   // η handleToggleComment θα καλείται όταν ο χρήστης πατάει το κουμπί
   // για να ανοίξει/κλείσει τα σχόλια ενός post
-  const handleToggleComment = (postId) => {
+  const handleToggleComment = (event, postId) => {
+    event.preventDefault();
+
     // αν το postId υπάρχει στον πίνακα expandedComments
     if (expandedComments.includes(postId)) {
       // τότε το αφαιρούμε
@@ -51,91 +53,100 @@ function App() {
     }
   };
 
-  // η getInitials επιστρέφει τα αρχικά ενός ονόματος
-  // π.χ. για τον χρήστη "John Doe" επιστρέφει "JD"
-  const getInitials = (name) => {
-    const [firstName, lastName] = name.split(' ');
-
-    return `${firstName[0]}${lastName[0]}`;
-  };
-
   return (
-    <div>
-      <header className="fixed top-0 left-0 w-full z-50 bg-white shadow-md">
-        <div className="h-16 flex items-center max-w-7xl mx-auto px-6 justify-between">
-          <div className="font-medium text-lg">Blog Project</div>
+    <div className="flex min-h-screen bg-gray-100">
+      <header className="fixed top-0 left-0 w-full z-50 bg-white shadow">
+        <div className="h-16 flex items-center max-w-6xl mx-auto px-6 justify-between">
+          <div className="font-semibold text-xl">Blog Project</div>
           {user ? (
             // αν ο χρήστης έχει κάνει login
             // δηλαδή έχουμε τα δεδομένα του στο user
             // τότε εμφανίζουμε το κουμπί με τα αρχικά του
             // π.χ. αν ο χρήστης έχει το όνομα "John Doe"
             // τότε το κουμπί θα έχει τα αρχικά "JD"
-            <button className="h-9 w-9 bg-blue-500 rounded-full text-white">{getInitials(user.name)}</button>
+            <button className="flex items-center justify-center text-center h-10 w-10 bg-blue-500 rounded-full text-white font-semibold">
+              {getInitials(user.name)}
+            </button>
           ) : // αλλιώς τίποτα
           null}
         </div>
       </header>
-      <main>
+      <main className="max-w-6xl mx-auto grow p-6">
         <div className="h-16" />
-        <div className="space-y-2 mt-8">
+        <div className="space-y-6 mt-6">
           {
             // για κάθε post
             posts.map((post) => (
               // εμφανίζουμε τον τίτλο και το κείμενο του
               // καθώς και τα σχόλια και τα likes
-              // το κουμπί για τα σχόλια έχει τον αριθμό των σχολίων
               // το κουμπί για τα likes έχει τον αριθμό των likes
               // και είναι διαφορετικό αν ο χρήστης έχει κάνει like
               // ή όχι στο post
               // το κουμπί για τα σχόλια έχει τον αριθμό των σχολίων
-              // και εμφανίζει τα σχόλια αν είναι ανοιχτά
-              <div key={post.id} className="max-w-7xl mx-auto px-6 py-4 bg-white rounded-md">
-                <div className="font-medium text-lg">{post.title}</div>
+              // και εμφανίζει τα σχόλια αν είναι κλειστά
+              <div key={post.id} className="p-6 bg-white rounded-lg shadow">
+                <div className="font-semibold text-lg mb-3">{post.title}</div>
                 <div className="text-gray-500">{post.body}</div>
-                <div className="flex items-center space-x-6 mt-4">
-                  <button onClick={() => handleToggleComment(post.id)}>
-                    <BiComment /> {post.comments.length}
-                  </button>
-                  <button>
+                <div className="flex items-center justify-between mt-6">
+                  <button className="inline-flex items-center text-sm text-gray-500">
                     {
                       // ο χρήστης έχει κάνει like στο post
                       // ανάλογα με αυτό εμφανίζουμε το κατάλληλο εικονίδιο
                       // και τον αριθμό των likes
                       // π.χ. αν ο χρήστης έχει κάνει like το post έχουμε:
-                      // <AiFillLike size={20} /> 1
+                      // <AiFillLike /> 1
                       // αλλιώς έχουμε:
-                      // <AiOutlineLike size={20} /> 0
+                      // <AiOutlineLike /> 0
                       post.likes.some((like) => like.userId === user.id) ? (
-                        <AiFillLike size={20} />
+                        <AiFillLike className="fill-blue-500 shrink-0" size={20} />
                       ) : (
-                        <AiOutlineLike size={20} />
+                        <AiOutlineLike className="shrink-0" size={20} />
                       )
                     }
+                    &nbsp;
                     {post.likes.length}
                   </button>
+
+                  <a
+                    className="text-sm text-gray-500 hover:underline"
+                    href="#"
+                    onClick={(event) => handleToggleComment(event, post.id)}
+                  >
+                    {post.comments.length} comments
+                  </a>
                 </div>
                 {
                   // αν το post έχει ανοιχτά τα σχόλια
                   // τότε εμφανίζουμε τα σχόλια
                   // αλλιώς τίποτα
                   expandedComments.includes(post.id) ? (
-                    <ul>
+                    <ul className="flex flex-col items-start space-y-3 py-3 mt-6">
                       {
                         // για κάθε σχόλιο
                         post.comments.map((comment) => {
                           // βρίσκουμε τον χρήστη που έκανε το σχόλιο
                           const commentUser = users.find((user) => user.id === comment.userId);
+                          const initials = commentUser ? getInitials(commentUser.name) : '';
 
                           return (
                             // εμφανίζουμε το σχόλιο
-                            <li key={comment.id}>
-                              <div className="text-sm text-gray-500">{comment.body}</div>
-                              {
-                                // αν έχουμε τον χρήστη που έκανε το σχόλιο
-                                // τότε εμφανίζουμε το όνομά του
-                                // αλλιώς τίποτα
-                                commentUser ? <div className="text-sm mt-1">{commentUser.name}</div> : null
-                              }
+                            // και το όνομα του χρήστη που το έκανε
+                            <li className="relative pl-10" key={comment.id}>
+                              <div
+                                className="absolute flex items-center justify-center text-center text-sm left-0 top-0 h-8 w-8 text-white rounded-full font-semibold"
+                                style={{ backgroundColor: getInitialsColor(initials) }}
+                              >
+                                {initials}
+                              </div>
+                              <div className="bg-gray-50 px-3 py-1.5 rounded-lg">
+                                {
+                                  // αν έχουμε τον χρήστη που έκανε το σχόλιο
+                                  // τότε εμφανίζουμε το όνομά του
+                                  // αλλιώς τίποτα
+                                  commentUser ? <div className="text-sm font-semibold">{commentUser.name}</div> : null
+                                }
+                                <div className="text-sm text-gray-500">{comment.body}</div>
+                              </div>
                             </li>
                           );
                         })
---
+++ src/utils.js
@@ -0,0 +1,38 @@
+// η getInitials επιστρέφει τα αρχικά ενός ονόματος
+// π.χ. για τον χρήστη "John Doe" επιστρέφει "JD"
+export function getInitials(name) {
+  const [firstName, lastName] = name.split(' ');
+  return `${firstName[0]}${lastName[0]}`;
+}
+
+// επιστρέφει μία τυχαία χρωματική τιμή
+// ανάλογα με τα αρχικά του ονόματος
+// https://stackoverflow.com/a/16348977/1825390
+export function getInitialsColor(initials) {
+  const colors = [
+    '#f44336',
+    '#e91e63',
+    '#9c27b0',
+    '#673ab7',
+    '#3f51b5',
+    '#2196f3',
+    '#03a9f4',
+    '#00bcd4',
+    '#009688',
+    '#4caf50',
+    '#8bc34a',
+    '#cddc39',
+    '#ffeb3b',
+    '#ffc107',
+    '#ff9800',
+    '#ff5722',
+    '#795548',
+    '#9e9e9e',
+    '#607d8b',
+  ];
+  const charCodeSum = initials
+    .split('')
+    .map((char) => char.charCodeAt(0))
+    .reduce((sum, charCode) => sum + charCode, 0);
+  return colors[charCodeSum % colors.length];
+}
