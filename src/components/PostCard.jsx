4f7b53d99acdf77bed7b2962f740debe90a3a6cb
Ioannis Potouridis <potouridisio@gmail.com>
2023-05-10 19:32:31 +0300

    feat: add the post details dialog

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useContext, useState } from 'react';
+import { createPortal } from 'react-dom';
 import { AiFillHeart, AiOutlineHeart, AiOutlineMore } from 'react-icons/ai';
 
 import { SessionContext } from '../App';
@@ -37,6 +38,8 @@ export default function PostCard({
   const [expandedComments, setExpandedComments] = useState(false);
   // το isOpen είναι το id του σχολίου που είναι ανοιχτό το μενού του διαφορετικά είναι false
   const [isOpen, setIsOpen] = useState(false);
+  //
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   const handleDelete = (commentId) => {
     onDeleteComment(commentId);
@@ -59,109 +62,132 @@ export default function PostCard({
   const postUser = users.find((user) => user.id === userId);
 
   return (
-    <div className="rounded-lg bg-white p-6 shadow">
-      <div className="mb-6 flex items-center">
-        <Avatar className="mr-2">{postUser.name}</Avatar>
-        <div>
-          <h3 className="text-sm font-semibold">{postUser.name}</h3>
-          <p className="text-xs text-gray-500">{timeAgo(new Date(createdAt))}</p>
+    <>
+      <div className="rounded-lg bg-white p-6 shadow">
+        <div className="mb-6 flex items-center">
+          <Avatar className="mr-2">{postUser.name}</Avatar>
+          <div>
+            <h3 className="text-sm font-semibold">{postUser.name}</h3>
+            <p className="text-xs text-gray-500">{timeAgo(new Date(createdAt))}</p>
+          </div>
         </div>
-      </div>
-      <h2 className="mb-2 text-lg font-semibold">{title}</h2>
-      <p className="text-sm text-gray-500">
-        {expandedBody ? body : truncate(body)}&nbsp;
-        {!expandedBody ? (
-          <a className="cursor-pointer font-medium hover:underline" href="#" onClick={handleSeeMore}>
-            See more
+        <h2 className="mb-2 cursor-pointer text-lg font-semibold hover:underline" onClick={() => setIsDialogOpen(true)}>
+          {title}
+        </h2>
+        <p className="text-sm text-gray-500">
+          {expandedBody ? body : truncate(body)}&nbsp;
+          {!expandedBody ? (
+            <a className="cursor-pointer font-medium hover:underline" href="#" onClick={handleSeeMore}>
+              See more
+            </a>
+          ) : null}
+        </p>
+        <div className="mt-6 flex items-center justify-between">
+          <button className="inline-flex select-none items-center text-sm text-gray-500" onClick={onLike}>
+            {likes.some((like) => like.userId === session.user.id) ? (
+              <AiFillHeart className="shrink-0 fill-red-500" size={20} />
+            ) : (
+              <AiOutlineHeart className="shrink-0 fill-gray-500" size={20} />
+            )}
+            &nbsp;
+            {likes.length}
+          </button>
+          <a
+            className="cursor-pointer select-none text-sm text-gray-500 hover:underline"
+            href="#"
+            onClick={handleXComments}
+          >
+            {comments.length} comments
           </a>
-        ) : null}
-      </p>
-      <div className="mt-6 flex items-center justify-between">
-        <button className="inline-flex select-none items-center text-sm text-gray-500" onClick={onLike}>
-          {likes.some((like) => like.userId === session.user.id) ? (
-            <AiFillHeart className="shrink-0 fill-red-500" size={20} />
-          ) : (
-            <AiOutlineHeart className="shrink-0 fill-gray-500" size={20} />
-          )}
-          &nbsp;
-          {likes.length}
-        </button>
-        <a
-          className="cursor-pointer select-none text-sm text-gray-500 hover:underline"
-          href="#"
-          onClick={handleXComments}
-        >
-          {comments.length} comments
-        </a>
-      </div>
-      {expandedComments ? (
-        <>
-          <hr className="mb-4 mt-6 border-t-gray-200" />
-          <ul className="flex flex-col items-start space-y-3 py-4">
-            {comments.map((comment) => {
-              const commentUser = users.find((user) => user.id === comment.userId);
+        </div>
+        {expandedComments ? (
+          <>
+            <hr className="mb-4 mt-6 border-t-gray-200" />
+            <ul className="flex flex-col items-start space-y-3 py-4">
+              {comments.map((comment) => {
+                const commentUser = users.find((user) => user.id === comment.userId);
 
-              // αν το σχόλιο έχει γίνει από τον χρήστη που έχει κάνει login
-              // ή το post έχει γίνει από τον χρήστη που έχει κάνει login
-              // ή αν ο χρήστης που έχει κάνει login είναι admin
-              // τότε μπορεί να διαγράψει το σχόλιο
-              const canDelete =
-                comment.userId === session.user.id || userId === session.user.id || session.user.type === 'admin';
+                // αν το σχόλιο έχει γίνει από τον χρήστη που έχει κάνει login
+                // ή το post έχει γίνει από τον χρήστη που έχει κάνει login
+                // ή αν ο χρήστης που έχει κάνει login είναι admin
+                // τότε μπορεί να διαγράψει το σχόλιο
+                const canDelete =
+                  comment.userId === session.user.id || userId === session.user.id || session.user.type === 'admin';
 
-              return (
-                <li className="group flex" key={comment.id}>
-                  <Avatar className="mr-2" size="small">
-                    {commentUser.name}
-                  </Avatar>
-                  <div className="flex flex-col items-end">
-                    <div className="rounded-lg bg-gray-100 px-3 py-1.5">
-                      {commentUser ? <h4 className="text-sm font-semibold">{commentUser.name}</h4> : null}
-                      <p className="text-sm text-gray-500">{comment.body}</p>
+                return (
+                  <li className="group flex" key={comment.id}>
+                    <Avatar className="mr-2" size="small">
+                      {commentUser.name}
+                    </Avatar>
+                    <div className="flex flex-col items-end">
+                      <div className="rounded-lg bg-gray-100 px-3 py-1.5">
+                        {commentUser ? <h4 className="text-sm font-semibold">{commentUser.name}</h4> : null}
+                        <p className="text-sm text-gray-500">{comment.body}</p>
+                      </div>
+                      <p className="mr-2 mt-0.5 text-xs text-gray-500">{timeAgo(new Date(comment.createdAt))}</p>
                     </div>
-                    <p className="mr-2 mt-0.5 text-xs text-gray-500">{timeAgo(new Date(comment.createdAt))}</p>
-                  </div>
-                  {canDelete ? (
-                    <div className="relative -mt-[1.125rem] ml-0.5 self-center">
-                      <button
-                        className="rounded-full p-1.5 hover:bg-gray-100"
-                        onClick={() => {
-                          setIsOpen(comment.id);
-                        }}
-                        tabIndex={-1}
-                      >
-                        <AiOutlineMore
-                          className="rotate-90 fill-gray-500 opacity-0 group-hover:opacity-100"
-                          size={20}
-                        />
-                      </button>
-                      <div
-                        className={`absolute left-1/2 z-10 mt-2${
-                          isOpen === comment.id ? '' : ' hidden'
-                        } w-32 -translate-x-1/2 transform rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5`}
-                      >
-                        <div className="py-1.5">
-                          <a
-                            className="block cursor-pointer px-3 py-1.5 text-sm font-medium text-gray-500 hover:bg-gray-100"
-                            onClick={() => handleDelete(comment.id)}
-                          >
-                            Delete
-                          </a>
+                    {canDelete ? (
+                      <div className="relative -mt-[1.125rem] ml-0.5 self-center">
+                        <button
+                          className="rounded-full p-1.5 hover:bg-gray-100"
+                          onClick={() => {
+                            setIsOpen(comment.id);
+                          }}
+                          tabIndex={-1}
+                        >
+                          <AiOutlineMore
+                            className="rotate-90 fill-gray-500 opacity-0 group-hover:opacity-100"
+                            size={20}
+                          />
+                        </button>
+                        <div
+                          className={`absolute left-1/2 z-10 mt-2${
+                            isOpen === comment.id ? '' : ' hidden'
+                          } w-32 -translate-x-1/2 transform rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5`}
+                        >
+                          <div className="py-1.5">
+                            <a
+                              className="block cursor-pointer px-3 py-1.5 text-sm font-medium text-gray-500 hover:bg-gray-100"
+                              onClick={() => handleDelete(comment.id)}
+                            >
+                              Delete
+                            </a>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  ) : null}
-                </li>
-              );
-            })}
-            <li className="flex w-full">
-              <Avatar className="mr-2" size="small">
-                {session.user.name}
-              </Avatar>
-              <CommentForm onSubmit={onComment} />
-            </li>
-          </ul>
-        </>
-      ) : null}
-    </div>
+                    ) : null}
+                  </li>
+                );
+              })}
+              <li className="flex w-full">
+                <Avatar className="mr-2" size="small">
+                  {session.user.name}
+                </Avatar>
+                <CommentForm onSubmit={onComment} />
+              </li>
+            </ul>
+          </>
+        ) : null}
+      </div>
+
+      {/* Modal ή Dialog */}
+      {isDialogOpen
+        ? createPortal(
+            <>
+              <div
+                className="fixed left-0 top-0 z-50 h-full w-full bg-gray-700 opacity-75"
+                onClick={() => setIsDialogOpen(false)}
+              />
+              <div className="fixed left-1/2 top-1/2 z-50 -translate-x-1/2 -translate-y-1/2 transform rounded-lg bg-white shadow-lg">
+                <div className="p-6">
+                  <h2 className="mb-2 text-lg font-semibold">{title}</h2>
+                  <p className="text-sm text-gray-500">{body}</p>
+                </div>
+              </div>
+            </>,
+            document.body,
+          )
+        : null}
+    </>
   );
 }
