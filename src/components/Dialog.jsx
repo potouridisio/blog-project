e1b4f13fe7706f0399bd8ac0308c16dc1089de3b
Ioannis Potouridis <potouridisio@gmail.com>
2023-05-12 16:14:22 +0300

    feat: add className prop to Dialog component

--- src/components/Dialog.jsx
+++ src/components/Dialog.jsx
@@ -4,9 +4,10 @@ import Portal from './Portal';
 
 // το Dialog δέχεται τα παρακάτω props:
 // children: τα παιδιά του component
+// className: το className του component
 // open: true αν είναι ανοιχτό το modal διαφορετικά false
 // onClose: η συνάρτηση που καλείται όταν κλείνει το modal
-export default function Dialog({ children, open, onClose }) {
+export default function Dialog({ children, className, open, onClose }) {
   // αν το open είναι false επιστρέφουμε null
   if (!open) {
     return null;
@@ -15,7 +16,11 @@ export default function Dialog({ children, open, onClose }) {
   return (
     <Portal>
       <div className="fixed left-0 top-0 z-50 h-full w-full bg-gray-700 opacity-75" onClick={onClose} />
-      <div className="fixed left-1/2 top-1/2 z-50 -translate-x-1/2 -translate-y-1/2 transform rounded-lg bg-white shadow-lg">
+      <div
+        className={`fixed left-1/2 top-1/2 z-50 -translate-x-1/2 -translate-y-1/2 transform rounded-lg bg-white shadow-lg${
+          className ? ` ${className}` : ''
+        }`}
+      >
         {children}
       </div>
     </Portal>
