068375e69bf11acc24e2905a8acbb090948e820e
Ioannis Potouridis <potouridisio@gmail.com>
2023-05-12 16:35:09 +0300

    feat: show oldest comments first

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -99,61 +99,63 @@ export default function PostCard({
           <>
             <hr className="mb-4 mt-6 border-t-gray-400 border-opacity-50" />
             <ul className="flex flex-col items-start space-y-3 py-4">
-              {comments.map((comment) => {
-                const commentUser = users.find((user) => user.id === comment.userId);
+              {comments
+                .sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
+                .map((comment) => {
+                  const commentUser = users.find((user) => user.id === comment.userId);
 
-                // αν το σχόλιο έχει γίνει από τον χρήστη που έχει κάνει login
-                // ή το post έχει γίνει από τον χρήστη που έχει κάνει login
-                // ή αν ο χρήστης που έχει κάνει login είναι admin
-                // τότε μπορεί να διαγράψει το σχόλιο
-                const canDelete =
-                  comment.userId === session.user.id || userId === session.user.id || session.user.type === 'admin';
+                  // αν το σχόλιο έχει γίνει από τον χρήστη που έχει κάνει login
+                  // ή το post έχει γίνει από τον χρήστη που έχει κάνει login
+                  // ή αν ο χρήστης που έχει κάνει login είναι admin
+                  // τότε μπορεί να διαγράψει το σχόλιο
+                  const canDelete =
+                    comment.userId === session.user.id || userId === session.user.id || session.user.type === 'admin';
 
-                return (
-                  <li className="group flex" key={comment.id}>
-                    <Avatar className="mr-2" size="small">
-                      {commentUser.name}
-                    </Avatar>
-                    <div className="flex flex-col items-end">
-                      <div className="rounded-lg bg-gray-100 px-3 py-1.5">
-                        {commentUser ? <p className="text-sm font-semibold">{commentUser.name}</p> : null}
-                        <p className="text-sm text-gray-500">{comment.body}</p>
+                  return (
+                    <li className="group flex" key={comment.id}>
+                      <Avatar className="mr-2" size="small">
+                        {commentUser.name}
+                      </Avatar>
+                      <div className="flex flex-col items-end">
+                        <div className="rounded-lg bg-gray-100 px-3 py-1.5">
+                          {commentUser ? <p className="text-sm font-semibold">{commentUser.name}</p> : null}
+                          <p className="text-sm text-gray-500">{comment.body}</p>
+                        </div>
+                        <p className="mr-2 mt-0.5 text-xs text-gray-500">{timeAgo(new Date(comment.createdAt))}</p>
                       </div>
-                      <p className="mr-2 mt-0.5 text-xs text-gray-500">{timeAgo(new Date(comment.createdAt))}</p>
-                    </div>
-                    {canDelete ? (
-                      <div className="relative -mt-[1.125rem] ml-0.5 self-center">
-                        <button
-                          className="rounded-full p-1.5 hover:bg-gray-100"
-                          onClick={() => {
-                            setIsOpen(comment.id);
-                          }}
-                          tabIndex={-1}
-                        >
-                          <AiOutlineMore
-                            className="rotate-90 fill-gray-500 opacity-0 group-hover:opacity-100"
-                            size={20}
-                          />
-                        </button>
-                        <div
-                          className={`absolute left-1/2 z-10 mt-2${
-                            isOpen === comment.id ? '' : ' hidden'
-                          } w-32 -translate-x-1/2 transform rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5`}
-                        >
-                          <div className="py-1.5">
-                            <a
-                              className="block cursor-pointer px-3 py-1.5 text-sm font-medium text-gray-500 hover:bg-gray-100"
-                              onClick={() => handleDelete(comment.id)}
-                            >
-                              Delete
-                            </a>
+                      {canDelete ? (
+                        <div className="relative -mt-[1.125rem] ml-0.5 self-center">
+                          <button
+                            className="rounded-full p-1.5 hover:bg-gray-100"
+                            onClick={() => {
+                              setIsOpen(comment.id);
+                            }}
+                            tabIndex={-1}
+                          >
+                            <AiOutlineMore
+                              className="rotate-90 fill-gray-500 opacity-0 group-hover:opacity-100"
+                              size={20}
+                            />
+                          </button>
+                          <div
+                            className={`absolute left-1/2 z-10 mt-2${
+                              isOpen === comment.id ? '' : ' hidden'
+                            } w-32 -translate-x-1/2 transform rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5`}
+                          >
+                            <div className="py-1.5">
+                              <a
+                                className="block cursor-pointer px-3 py-1.5 text-sm font-medium text-gray-500 hover:bg-gray-100"
+                                onClick={() => handleDelete(comment.id)}
+                              >
+                                Delete
+                              </a>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    ) : null}
-                  </li>
-                );
-              })}
+                      ) : null}
+                    </li>
+                  );
+                })}
               <li className="flex w-full">
                 <Avatar className="mr-2" size="small">
                   {session.user.name}
