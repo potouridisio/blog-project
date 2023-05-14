5d55283118afd1d300bb2b1d3cf58e7f5d2ec1dc
Konstantinos Pavlos <kpav1991@gmail.com>
2023-05-15 01:41:37 +0300

    Add Cancel, Escape key and Handle Edit of a comment

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -2,10 +2,18 @@
 import { useState } from 'react';
 import { AiOutlineSend } from 'react-icons/ai';
 
-// η CommentForm δέχεται το παρακάτω prop:
-// initialValue: το αρχικό περιεχόμενο του input
-// onSubmit: η συνάρτηση που καλείται με το σχόλιο όταν γίνεται submit της φόρμας
-export default function CommentForm({ initialValue, onSubmit }) {
+/**
+ * A form for submitting comments.
+ *
+ * @typedef {object} Props
+ * @property {string} initialValue - The initial value of the input.
+ * @property {(comment: string) => void} onSubmit - The function that will be called with the comment when the form is submitted.
+ *
+ * @param {Props} props - The props object.
+ *
+ * @returns {JSX.Element} - The rendered CommentForm component.
+ */
+export default function CommentForm({ initialValue, onSubmit, onCancelClick, onEscapeDown }) {
   // το value είναι το περιεχόμενο του input
   const [value, setValue] = useState(initialValue ?? '');
 
@@ -21,32 +29,45 @@ export default function CommentForm({ initialValue, onSubmit }) {
   // το isEditing είναι true αν υπάρχει initialValue που σημαίνει ότι ο χρήστης επεξεργάζεται ένα σχόλιο
   const isEditing = initialValue !== undefined;
 
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
     <div className="flex w-full flex-col">
-      <form className="w-full rounded-lg bg-gray-100 py-0.5" onSubmit={handleSubmit}>
+      <form className="w-full rounded-lg bg-gray-100" onSubmit={handleSubmit}>
         <input
           autoComplete="off"
           autoFocus
-          className="w-full bg-transparent px-3 py-1.5 text-sm text-inherit placeholder-gray-500 focus:outline-none"
+          className="w-full bg-transparent p-2 text-sm text-inherit placeholder-gray-500 focus:outline-none"
           name="comment"
           onChange={(event) => setValue(event.target.value)}
+          onFocus={(e) => {
+            e.stopPropagation();
+            setIsFocused(true);
+          }}
+          onBlur={() => setIsFocused(false)}
+          onKeyDown={onEscapeDown}
           placeholder="Write a comment..."
           value={value}
         />
-        <div className="flex items-center justify-end px-3 pb-2">
-          <button className="-mb-1 -mr-1.5 rounded-full p-1.5 hover:bg-gray-200" type="submit">
+        <div className="flex items-center justify-end p-2 pt-0">
+          <button className="-mb-1 -mr-1 rounded-full p-1.5 hover:bg-gray-200" type="submit">
             <AiOutlineSend className="fill-gray-500" size={20} />
           </button>
         </div>
       </form>
-      {isEditing ? (
+
+      {isEditing && isFocused ? (
         <p className="mr-2 mt-0.5 text-xs text-gray-500">
           Press Esc to{' '}
-          <a className="text-blue-500 hover:underline" href="#">
+          <a className="text-blue-500 hover:underline" href="#" onClick={onCancelClick}>
             cancel
           </a>
           .
         </p>
+      ) : isEditing ? (
+        <a className="w-2 text-xs text-blue-500 hover:underline" href="#" onClick={onCancelClick}>
+          Cancel
+        </a>
       ) : null}
     </div>
   );
--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -9,25 +9,42 @@ import CommentForm from './CommentForm';
 import Dialog from './Dialog';
 import Popper from './Popper';
 
-// η PostCard δέχεται τα παρακάτω props:
-// body: το κείμενο του post
-// comments: τα σχόλια του post
-// likes: τα likes του post
-// onComment: η συνάρτηση που καλείται με το σχόλιο όταν γίνεται submit της φόρμας
-// onDeleteComment: η συνάρτηση που καλείται όταν διαγράφεται ένα σχόλιο
-// onEditComment: η συνάρτηση που καλείται με το νέο σχόλιο όταν γίνεται edit ενός σχολίου
-// onLike: η συνάρτηση που καλείται όταν γίνεται like στο post
-// title: ο τίτλος του post
-// userId: το id του χρήστη που έκανε το post
-// users: οι χρήστες της εφαρμογής
+/**
+ * A component that displays a post with its body, comments, likes, title and user information.
+ *
+ * @typedef {object} Props
+ * @property {string} body - The body text of the post.
+ * @property {object[]} comments - An array of comments associated with the post.
+ * @property {string} comments[].id - The ID of the comment.
+ * @property {string} comments[].text - The text of the comment.
+ * @property {string} comments[].createdAt - The creation date of the comment.
+ * @property {string} comments[].updatedAt - The last update date of the comment.
+ * @property {string} createdAt - The creation date of the post.
+ * @property {number} likes - The number of likes the post has received.
+ * @property {(comment: string) => void} onComment - A function that is called when the comment form is submitted.
+ * @property {() => void} onDelete - A function that is called when the post is deleted.
+ * @property {(commentId: string) => void} onDeleteComment - A function that is called when a comment is deleted.
+ * @property {(commentId: string, newText: string) => void} onEditComment - A function that is called when a comment is edited.
+ * @property {() => void} onLike - A function that is called when the like button is clicked.
+ * @property {string} title - The title of the post.
+ * @property {string} userId - The ID of the user who made the post.
+ * @property {object[]} users - An array of users in the application.
+ * @property {string} users[].id - The ID of the user.
+ * @property {string} users[].name - The name of the user.
+ *
+ * @param {Props} props - The props object.
+ *
+ * @returns {JSX.Element} - The rendered PostCard component.
+ */
 export default function PostCard({
   body,
   comments,
   createdAt,
   likes,
   onComment,
+  onDelete,
   onDeleteComment,
-  // onEditComment
+  //onEditComment,
   onLike,
   title,
   userId,
@@ -39,10 +56,12 @@ export default function PostCard({
   const [expandedBody, setExpandedBody] = useState(false);
   // το expandedComments είναι true αν έχει γίνει κλικ στο "x comments"
   const [expandedComments, setExpandedComments] = useState(false);
-  // το isEditing είναι το id του σχολίου που επεξεργάζεται ο χρήστης ή false αν δεν επεξεργάζεται κάποιο σχόλιο
-  const [isEditing, setIsEditing] = useState(false);
-  // το isDeleting είναι το id του σχολίου που θα διαγραφεί ή false αν δεν θα διαγραφεί κάποιο σχόλιο
-  const [isDeleting, setIsDeleting] = useState(false);
+  // το isEditingComment είναι το id του σχολίου που επεξεργάζεται ο χρήστης ή false αν δεν επεξεργάζεται κάποιο σχόλιο
+  const [isEditingComment, setIsEditingComment] = useState(false);
+  // το isDeletingComment είναι το id του σχολίου που θα διαγραφεί ή false αν δεν θα διαγραφεί κάποιο σχόλιο
+  const [isDeletingComment, setIsDeletingComment] = useState(false);
+  // // το isDeletingPost είναι true αν έχει γίνει κλικ στο "Delete Post"
+  const [isDeletingPost, setIsDeletingPost] = useState(false);
 
   // // η handleXComments καλείται όταν γίνεται κλικ στο "x comments"
   const handleXComments = (event) => {
@@ -56,20 +75,55 @@ export default function PostCard({
     setExpandedBody(true);
   };
 
+  const handleCancelClick = (e) => {
+    e.preventDefault();
+    setIsEditingComment(false);
+  };
+
+  const handlePressEscape = (e) => {
+    if (e.keyCode === 27) {
+      setIsEditingComment(false);
+    }
+  };
+
   // βρίσκουμε τον χρήστη που έκανε το post
   const postUser = users.find((user) => user.id === userId);
+
+  // το canDeletePost είναι true αν ο χρήστης είναι ο δημιουργός του post ή είναι admin
+  const canDeletePost = userId === session.user.id || session.user.role === 'admin';
 
   return (
     <>
       <div className="rounded-lg bg-white p-6 shadow">
         <div className="mb-6 flex items-center">
           <Avatar className="mr-2">{postUser.name}</Avatar>
-          <div>
+          <div className="grow">
             <p className="text-sm font-semibold">{postUser.name}</p>
             <p className="text-xs text-gray-500">{timeAgo(new Date(createdAt))}</p>
           </div>
+
+          {canDeletePost ? (
+            <Popper
+              trigger={
+                <button className="rounded-full p-1.5 hover:bg-gray-100">
+                  <AiOutlineMore className="rotate-90 fill-gray-500" size={24} />
+                </button>
+              }
+            >
+              <div className="p-2">
+                <a
+                  className="block cursor-pointer rounded px-3 py-1.5 text-sm font-medium hover:bg-gray-100"
+                  onClick={() => setIsDeletingPost(true)}
+                >
+                  Delete
+                </a>
+              </div>
+            </Popper>
+          ) : null}
         </div>
+
         <h2 className="mb-2 text-lg font-semibold">{title}</h2>
+
         <p className="text-sm text-gray-500">
           {expandedBody ? body : truncate(body)}&nbsp;
           {!expandedBody ? (
@@ -78,6 +132,7 @@ export default function PostCard({
             </a>
           ) : null}
         </p>
+
         <div className="mt-6 flex items-center justify-between">
           <button className="inline-flex select-none items-center text-sm text-gray-500" onClick={onLike}>
             {likes.some((like) => like.userId === session.user.id) ? (
@@ -88,6 +143,7 @@ export default function PostCard({
             &nbsp;
             {likes.length}
           </button>
+
           <a
             className="cursor-pointer select-none text-sm text-gray-500 hover:underline"
             href="#"
@@ -109,63 +165,66 @@ export default function PostCard({
                     comment.userId === session.user.id || userId === session.user.id || session.user.role === 'admin';
                   // το canEditComment είναι true αν ο χρήστης είναι ο δημιουργός του σχολίου
                   const canEditComment = comment.userId === session.user.id;
-                  // το showPopper είναι true αν δεν επεξεργάζεται κάποιο σχόλιο και ο χρήστης μπορεί να διαγράψει ή να επεξεργαστεί το σχόλιο
-                  const showPopper = !isEditing && (canDeleteComment || canEditComment);
 
                   return (
-                    <li className={`group flex${isEditing === comment.id ? ' w-full' : ''}`} key={comment.id}>
+                    <li className={`group flex${isEditingComment === comment.id ? ' w-full' : ''}`} key={comment.id}>
                       <Avatar className="mr-2" size="small">
                         {commentUser.name}
                       </Avatar>
-                      {isEditing === comment.id ? (
+
+                      {isEditingComment === comment.id ? (
                         <div className="flex w-full flex-col">
                           <CommentForm
                             initialValue={comment.body}
                             onSubmit={(newComment) => {
-                              // καλούμε την onEditComment με το id του σχολίου και το νέο σχόλιο
-                              console.log({ newComment });
+                              comment.body = newComment;
+                              comment.createdAt = new Date().toISOString();
+                              setIsEditingComment(false);
                             }}
+                            onCancelClick={handleCancelClick}
+                            onEscapeDown={handlePressEscape}
                           />
                         </div>
                       ) : (
                         <div className="flex flex-col items-end">
-                          <div className="rounded-lg bg-gray-100 px-3 py-1.5">
+                          <div className="rounded-lg bg-gray-100 p-2">
                             {commentUser ? <p className="text-sm font-semibold">{commentUser.name}</p> : null}
                             <p className="text-sm text-gray-500">{comment.body}</p>
                           </div>
                           <p className="mr-2 mt-0.5 text-xs text-gray-500">{timeAgo(new Date(comment.createdAt))}</p>
                         </div>
                       )}
-                      {showPopper ? (
+
+                      {!isEditingComment && (canDeleteComment || canEditComment) ? (
                         <Popper
                           className="-mt-[1.125rem] ml-0.5 self-center"
-                          content={
-                            <div className="py-1.5">
-                              {canEditComment ? (
-                                <a
-                                  className="block cursor-pointer px-3 py-1.5 text-sm font-medium text-gray-500 hover:bg-gray-100"
-                                  onClick={() => setIsEditing(comment.id)}
-                                >
-                                  Edit
-                                </a>
-                              ) : null}
-                              {canDeleteComment ? (
-                                <a
-                                  className="block cursor-pointer px-3 py-1.5 text-sm font-medium text-gray-500 hover:bg-gray-100"
-                                  onClick={() => setIsDeleting(comment.id)}
-                                >
-                                  Delete
-                                </a>
-                              ) : null}
-                            </div>
+                          trigger={
+                            <button
+                              className="rounded-full p-1.5 opacity-0 hover:bg-gray-100 group-hover:opacity-100"
+                              tabIndex={-1}
+                            >
+                              <AiOutlineMore className="rotate-90 fill-gray-500" size={20} />
+                            </button>
                           }
                         >
-                          <button
-                            className="rounded-full p-1.5 opacity-0 hover:bg-gray-100 group-hover:opacity-100"
-                            tabIndex={-1}
-                          >
-                            <AiOutlineMore className="rotate-90 fill-gray-500" size={20} />
-                          </button>
+                          <div className="p-2">
+                            {canEditComment ? (
+                              <a
+                                className="block cursor-pointer rounded px-3 py-1.5 text-sm font-medium hover:bg-gray-100"
+                                onClick={() => setIsEditingComment(comment.id)}
+                              >
+                                Edit
+                              </a>
+                            ) : null}
+                            {canDeleteComment ? (
+                              <a
+                                className="block cursor-pointer rounded px-3 py-1.5 text-sm font-medium hover:bg-gray-100"
+                                onClick={() => setIsDeletingComment(comment.id)}
+                              >
+                                Delete
+                              </a>
+                            ) : null}
+                          </div>
                         </Popper>
                       ) : null}
                     </li>
@@ -182,7 +241,37 @@ export default function PostCard({
         ) : null}
       </div>
 
-      <Dialog className="w-full max-w-xl" onClose={() => setIsDeleting(false)} open={isDeleting !== false}>
+      <Dialog className="w-full max-w-xl" onClose={() => setIsDeletingPost(false)} open={isDeletingPost}>
+        <h3 className="border-b border-b-gray-400 border-opacity-50 p-6 font-semibold">Delete Post?</h3>
+        <div className="p-6">
+          <p className="text-sm text-gray-500">Are you sure you want to delete this post?</p>
+        </div>
+        <div className="flex justify-end p-6">
+          <button
+            className="min-w-[6rem] select-none rounded bg-transparent px-4 py-2 text-sm font-medium text-blue-500 hover:bg-gray-100"
+            onClick={() => setIsDeletingPost(false)}
+          >
+            No
+          </button>
+          <button
+            className="ml-2 min-w-[6rem] select-none rounded bg-blue-500 px-4 py-2 text-sm font-medium text-white hover:bg-blue-600"
+            onClick={() => {
+              // καλούμε την onDelete χωρίς παραμέτρους γιατί δεν χρειάζεται να ξέρουμε το id του post που θα διαγραφεί αφού έχουμε το post στο map του App
+              onDelete();
+              // κάνουμε reset το isDeletingComment για να κλείσει το modal
+              setIsDeletingPost(false);
+            }}
+          >
+            Delete
+          </button>
+        </div>
+      </Dialog>
+
+      <Dialog
+        className="w-full max-w-xl"
+        onClose={() => setIsDeletingComment(false)}
+        open={isDeletingComment !== false}
+      >
         <h3 className="border-b border-b-gray-400 border-opacity-50 p-6 font-semibold">Delete Comment?</h3>
         <div className="p-6">
           <p className="text-sm text-gray-500">Are you sure you want to delete this comment?</p>
@@ -190,7 +279,7 @@ export default function PostCard({
         <div className="flex justify-end p-6">
           <button
             className="min-w-[6rem] select-none rounded bg-transparent px-4 py-2 text-sm font-medium text-blue-500 hover:bg-gray-100"
-            onClick={() => setIsDeleting(false)}
+            onClick={() => setIsDeletingComment(false)}
           >
             No
           </button>
@@ -198,9 +287,9 @@ export default function PostCard({
             className="ml-2 min-w-[6rem] select-none rounded bg-blue-500 px-4 py-2 text-sm font-medium text-white hover:bg-blue-600"
             onClick={() => {
               // καλούμε την onDeleteComment με το id του σχολίου που θα διαγραφεί
-              onDeleteComment(isDeleting);
-              // κάνουμε reset το isDeleting για να κλείσει το modal
-              setIsDeleting(false);
+              onDeleteComment(isDeletingComment);
+              // κάνουμε reset το isDeletingComment για να κλείσει το modal
+              setIsDeletingComment(false);
             }}
           >
             Delete
