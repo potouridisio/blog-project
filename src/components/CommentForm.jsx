bbe397c0fa1e29d28f9d1ad47c3b4b686f5cc2d5
Ioannis Potouridis <potouridisio@gmail.com>
2023-05-15 17:05:56 +0300

    fix: do not fake input click on the toolbar

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { AiOutlineSend } from 'react-icons/ai';
 
 /**
@@ -14,8 +14,6 @@ import { AiOutlineSend } from 'react-icons/ai';
  * @returns {JSX.Element} - The rendered CommentForm component.
  */
 export default function CommentForm({ initialValue, onSubmit }) {
-  // το inputRef είναι ένα ref που θα χρησιμοποιήσουμε για να κάνουμε focus το input όταν ο χρήστης κάνει κλικ στο σχόλιο
-  const inputRef = useRef(null);
   // το value είναι το περιεχόμενο του input
   const [value, setValue] = useState(initialValue ?? '');
 
@@ -33,22 +31,19 @@ export default function CommentForm({ initialValue, onSubmit }) {
 
   return (
     <>
-      <form className="w-full rounded-lg bg-gray-100" onSubmit={handleSubmit}>
+      <form className="relative w-full" onSubmit={handleSubmit}>
         <input
           autoComplete="off"
           autoFocus
-          className="w-full bg-transparent p-2 text-sm text-inherit placeholder-gray-500 focus:outline-none"
+          className="w-full rounded-lg bg-gray-100 p-2 pb-11 pr-10 text-sm text-inherit placeholder-gray-500 focus:outline-none"
           name="comment"
           onChange={(event) => setValue(event.target.value)}
           placeholder="Write a comment..."
-          ref={inputRef}
           value={value}
         />
-        <div className="flex h-9 cursor-text items-center justify-end px-2" onClick={() => inputRef.current?.focus()}>
-          <button className="-mr-2 rounded-full p-1.5 hover:bg-gray-200" type="submit">
-            <AiOutlineSend className="fill-gray-500" size={20} />
-          </button>
-        </div>
+        <button className="absolute bottom-1 right-1 rounded-full p-1.5 hover:bg-gray-200" type="submit">
+          <AiOutlineSend className="fill-gray-500" size={20} />
+        </button>
       </form>
       {isEditing ? (
         <p className="ml-2 mt-0.5 text-xs text-gray-500">
