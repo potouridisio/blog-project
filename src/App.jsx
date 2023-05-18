61b7b1c7b538d44a221db07065ce8c0bb288eee8
Ioannis Potouridis <potouridisio@gmail.com>
2023-05-18 09:28:48 +0300

    docs: remove comments from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,50 @@
 import { useEffect, useState } from 'react';
-
 import Avatar from './components/Avatar';
 import PostCard from './components/PostCard';
 import { useAuth } from './lib/auth';
 import { usePosts, useUsers } from './lib/hooks';
 
 function App() {
-  // το session είναι το session του χρήστη που έχει κάνει login
   const session = useAuth();
-  // η usePosts() επιστρέφει ένα αντικείμενο με τις τιμές των isLoading και posts
   const { isLoading: isLoadingPosts, posts: initialPosts } = usePosts();
   const [posts, setPosts] = useState(initialPosts);
-  // η useUsers() επιστρέφει ένα αντικείμενο με τις τιμές των isLoading και users
   const { isLoading: isLoadingUsers, users } = useUsers();
-  // η isLoadingInitialData είναι true αν οι τιμές των isLoadingPosts και isLoadingUsers είναι true
   const isLoadingInitialData = isLoadingPosts || isLoadingUsers;
 
-  // η useEffect() καλείται όταν αλλάζει η τιμή του initialPosts
   useEffect(() => {
-    // αν η τιμή του initialPosts είναι διαφορετική από το posts τότε αντικαθιστούμε το posts με το initialPosts
     if (initialPosts) {
       setPosts(initialPosts);
     }
   }, [initialPosts]);
 
-  // η handleComment() δέχεται το σχόλιο και το index του post στο οποίο έγινε το σχόλιο
   const handleComment = (comment, postIndex) => {
-    // αν δεν υπάρχει σχόλιο τότε επιστρέφουμε undefined
     if (!comment) {
       return undefined;
     } else {
-      // αντιγράφουμε τον πίνακα posts
       const newPosts = posts.slice().map((obj) => Object.assign({}, obj));
-
-      // βρίσκουμε το post στο οποίο έγινε το σχόλιο
       const post = newPosts[postIndex];
 
-      // προσθέτουμε το σχόλιο στο post
       post.comments.push({
         body: comment,
-        // το createdAt του νέου σχολίου είναι η τρέχουσα ημερομηνία
         createdAt: new Date().toISOString(),
-        // το id του νέου σχολίου είναι ένα τυχαίο string
         id: crypto.randomUUID(),
-        // το userId του νέου σχολίου είναι το id του χρήστη που έκανε login
         userId: session.user.id,
       });
-
-      // αντικαθιστούμε τον πίνακα posts με τον νέο πίνακα posts
       setPosts(newPosts);
     }
   };
 
-  // η handleDeleteComment() δέχεται το id του σχολίου που διαγράφεται και το index του post στο οποίο έγινε το σχόλιο
   const handleDeleteComment = (commentId, postIndex) => {
-    // αντιγράφουμε τον πίνακα posts
     const newPosts = posts.slice().map((obj) => Object.assign({}, obj));
-    // βρίσκουμε το post στο οποίο έγινε το σχόλιο
     const post = newPosts[postIndex];
 
-    // αφαιρούμε το σχόλιο από το post
     post.comments = post.comments.filter((comment) => comment.id !== commentId);
 
-    // αντικαθιστούμε τον πίνακα posts με τον νέο πίνακα posts
     setPosts(newPosts);
   };
 
   const handleEditComment = (commentId, newComment, postIndex) => {
-    // αντιγράφουμε τον πίνακα posts
     const newPosts = posts.slice().map((obj) => Object.assign({}, obj));
-    // βρίσκουμε το post στο οποίο έγινε το σχόλιο
     const post = newPosts[postIndex];
 
     post.comments.forEach((comment) => {
@@ -81,32 +56,23 @@ function App() {
     setPosts(newPosts);
   };
 
-  // η handleLike() δέχεται το index του post που έγινε like
   const handleLike = (postIndex) => {
-    // αντιγράφουμε τον πίνακα posts
     const newPosts = posts.slice().map((obj) => Object.assign({}, obj));
-    // βρίσκουμε το post που έγινε like
     const post = newPosts[postIndex];
-    // βρίσκουμε το like του χρήστη στο post
     const like = post.likes.find((like) => like.userId === session.user.id);
 
-    // αν υπάρχει like του χρήστη στο post τότε το αφαιρούμε, αλλιώς το προσθέτουμε
     if (like) {
       post.likes = post.likes.filter((like) => like.userId !== session.user.id);
     } else {
       post.likes.push({
-        // το id του νέου like είναι ένα τυχαίο string
         id: crypto.randomUUID(),
-        // το userId του νέου like είναι το id του χρήστη που έκανε login
         userId: session.user.id,
       });
     }
 
-    // αντικαθιστούμε τον πίνακα posts με τον νέο πίνακα posts
     setPosts(newPosts);
   };
 
-  // αν οι τιμές των isLoadingSession, isLoadingPosts και isLoadingUsers είναι true τότε επιστρέφουμε το μήνυμα "Loading..."
   if (isLoadingInitialData) {
     return <div className="flex min-h-screen items-center justify-center">Loading...</div>;
   }
@@ -123,7 +89,6 @@ function App() {
         <div className="h-16" />
         <div className="space-y-6 py-6">
           {posts
-            // ταξινομούμε τα posts με βάση το createdAt για να βλέπουμε πρώτα τα πιο πρόσφατα
             .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
             .map(({ id, ...post }, index) => (
               <PostCard
