7c18c274c96e94198fc3588af88073c2120c76f6
Ioannis Potouridis <potouridisio@gmail.com>
2023-05-18 16:48:39 +0300

    chore: temporarily remove React.StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
 import './index.css';
 
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { AuthProvider } from './lib/auth';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>,
+  <AuthProvider>
+    <App />
+  </AuthProvider>,
 );
