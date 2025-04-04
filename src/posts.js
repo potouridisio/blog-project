3be272743de47851eb7c16a6e7429ee9cb854fce
Ioannis Potouridis <potouridisio@gmail.com>
2025-04-04 17:44:58 +0300

    Fetch and render post

--- src/posts.js
+++ src/posts.js
@@ -20,9 +20,26 @@ for (const post of posts) {
   const postLink = document.createElement("a");
 
   postLink.className =
-    "block px-4 py-2.5 text-sm hover:bg-indigo-300/70 focus:bg-indigo-300/70 focus:outline-0";
+    "block px-4 py-2.5 text-sm text-indigo-900 hover:bg-indigo-300/70 focus:bg-indigo-300/70 focus:outline-0";
   postLink.href = "#";
   postLink.textContent = post.title;
+  postLink.addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    const response = await fetch(`http://localhost:3000/posts/${post.id}`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+
+    const json = await response.json();
+
+    const postTitle = document.getElementById("postTitle");
+    const postContent = document.getElementById("postContent");
+
+    postTitle.textContent = json.title;
+    postContent.textContent = json.content;
+  });
 
   postsNav.appendChild(postLink);
 }
