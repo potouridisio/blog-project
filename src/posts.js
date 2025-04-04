22742dac8fdd1a62154b0276b847969e1b23c055
Ioannis Potouridis <potouridisio@gmail.com>
2025-04-04 17:32:18 +0300

    Fetch and render posts

--- src/posts.js
+++ src/posts.js
@@ -5,3 +5,24 @@ const token = localStorage.getItem("token");
 if (!token) {
   window.location.href = "/login";
 }
+
+const response = await fetch("http://localhost:3000/posts", {
+  headers: {
+    Authorization: token,
+  },
+});
+
+const posts = await response.json();
+
+const postsNav = document.getElementById("postsNav");
+
+for (const post of posts) {
+  const postLink = document.createElement("a");
+
+  postLink.className =
+    "block px-4 py-2.5 text-sm hover:bg-indigo-300/70 focus:bg-indigo-300/70 focus:outline-0";
+  postLink.href = "#";
+  postLink.textContent = post.title;
+
+  postsNav.appendChild(postLink);
+}
