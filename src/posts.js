f8e7e867c15c556d01a8245327a96621ba34fca7
rafail <petridisraf@windowslive.com>
2025-04-06 18:19:05 +0300

    Create function to render posts list

--- src/posts.js
+++ src/posts.js
@@ -17,39 +17,44 @@ const posts = await response.json();
 const postsNav = document.getElementById("postsNav");
 const removePostEl=document.getElementById("removePost");
 
-for (const post of posts) {
-  const postLink = document.createElement("a");
+function listPosts(){
   
-  postLink.id=post.id;
-  postLink.className =
-    "flex items-center justify-between px-4 py-2.5 text-sm text-indigo-900 hover:bg-indigo-300/70 focus:bg-indigo-300/70 focus:outline-0";
-  postLink.href = "#";
-  postLink.textContent = post.title;
-  postLink.addEventListener("click", async (event) => {
-    event.preventDefault();
+  postsNav.innerHTML="";
 
-    const response = await fetch(`http://localhost:3000/posts/${post.id}`, {
-      headers: {
-        Authorization: token,
-      },
+  for (const post of posts) {
+    const postLink = document.createElement("a");
+    
+    postLink.id=post.id;
+    postLink.className =
+      "flex items-center justify-between px-4 py-2.5 text-sm text-indigo-900 hover:bg-indigo-300/70 focus:bg-indigo-300/70 focus:outline-0";
+    postLink.href = "#";
+    postLink.textContent = post.title;
+    postLink.addEventListener("click", async (event) => {
+      event.preventDefault();
+
+      const response = await fetch(`http://localhost:3000/posts/${post.id}`, {
+        headers: {
+          Authorization: token,
+        },
+      });
+
+      const json = await response.json();
+
+      const postTitle = document.getElementById("postTitle");
+      const postContent = document.getElementById("postContent");
+
+      postTitle.textContent = json.title;
+      postContent.textContent = json.content;
+      //console.log("link")
     });
 
-    const json = await response.json();
+    postLink.addEventListener("mouseover",()=>{
+      postLink.appendChild(removePostEl);
+      removePostEl.classList.remove("invisible")
+    });
 
-    const postTitle = document.getElementById("postTitle");
-    const postContent = document.getElementById("postContent");
-
-    postTitle.textContent = json.title;
-    postContent.textContent = json.content;
-    //console.log("link")
-  });
-
-  postLink.addEventListener("mouseover",()=>{
-    postLink.appendChild(removePostEl);
-    removePostEl.classList.remove("invisible")
-  });
-
-  postsNav.appendChild(postLink);
+    postsNav.appendChild(postLink);
+  }
 }
 
 postsNav.addEventListener("mouseleave",()=>{
@@ -65,6 +70,8 @@ removePostEl.addEventListener("click",(event)=>{
       Authorization: token,
     },
   });
-
+  listPosts();
   // console.log("button");
 });
+
+listPosts();
