4943bb78cc77611c905524d72828c8c5714c4fc5
rafail <petridisraf@windowslive.com>
2025-04-06 18:07:29 +0300

    commit

--- src/posts.js
+++ src/posts.js
@@ -15,10 +15,12 @@ const response = await fetch("http://localhost:3000/posts", {
 const posts = await response.json();
 
 const postsNav = document.getElementById("postsNav");
+const removePostEl=document.getElementById("removePost");
 
 for (const post of posts) {
   const postLink = document.createElement("a");
-
+  
+  postLink.id=post.id;
   postLink.className =
     "flex items-center justify-between px-4 py-2.5 text-sm text-indigo-900 hover:bg-indigo-300/70 focus:bg-indigo-300/70 focus:outline-0";
   postLink.href = "#";
@@ -39,7 +41,30 @@ for (const post of posts) {
 
     postTitle.textContent = json.title;
     postContent.textContent = json.content;
+    //console.log("link")
+  });
+
+  postLink.addEventListener("mouseover",()=>{
+    postLink.appendChild(removePostEl);
+    removePostEl.classList.remove("invisible")
   });
 
   postsNav.appendChild(postLink);
 }
+
+postsNav.addEventListener("mouseleave",()=>{
+  removePostEl.classList.add("invisible")
+});
+removePostEl.addEventListener("click",(event)=>{
+  event.stopPropagation();
+  let postIdForDelete=removePostEl.parentElement.id;
+  console.log(postIdForDelete);
+  const deleteResponse = fetch(`http://localhost:3000/posts/${removePostEl.parentElement.id}`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: token,
+    },
+  });
+
+  // console.log("button");
+});
