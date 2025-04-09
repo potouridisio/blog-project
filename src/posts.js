b5b615d0796fb3dd5d651ae6aa58cc70823d5237
Ioannis Potouridis <potouridisio@gmail.com>
2025-04-09 19:10:44 +0300

    Move render functions to utils.js

--- src/posts.js
+++ src/posts.js
@@ -1,11 +1,7 @@
 import "./style.css";
 
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
-
-import { getComments, getPost, getPosts } from "./api";
-
-dayjs.extend(relativeTime);
+import { getPosts } from "./api";
+import { renderPosts } from "./utils";
 
 const token = localStorage.getItem("token");
 
@@ -15,44 +11,4 @@ if (!token) {
 
 const posts = await getPosts(token);
 
-const postsNav = document.getElementById("postsNav");
-
-for (const { id, title } of posts) {
-  const postLink = document.createElement("a");
-
-  postLink.className =
-    "block px-4 py-2.5 text-sm text-indigo-900 hover:bg-indigo-300/70 focus:bg-indigo-300/70 focus:outline-0";
-  postLink.href = "#";
-  postLink.textContent = title;
-  postLink.addEventListener("click", async (event) => {
-    event.preventDefault();
-
-    const post = await getPost(id, token);
-
-    const postTitle = document.getElementById("postTitle");
-    const postContent = document.getElementById("postContent");
-
-    postTitle.textContent = post.title;
-    postContent.textContent = post.content;
-
-    const comments = await getComments(post.id, token);
-
-    const commentsList = document.getElementById("commentsList");
-
-    commentsList.innerHTML = "";
-
-    for (const comment of comments) {
-      const commentListItem = document.createElement("li");
-
-      commentListItem.innerHTML = `
-        <h4 class="text-sm font-semibold text-indigo-700">${comment.author}</h4>
-        <p class="mb-2 text-xs text-indigo-500">${dayjs(comment.createdAt).fromNow()}</p>
-        <p class="text-sm text-indigo-900">${comment.content}</p>
-      `;
-
-      commentsList.appendChild(commentListItem);
-    }
-  });
-
-  postsNav.appendChild(postLink);
-}
+renderPosts(posts, token);
---
+++ src/utils.js
@@ -0,0 +1,58 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+import { getComments, getPost } from "./api";
+
+dayjs.extend(relativeTime);
+
+export function renderPosts(posts, token) {
+  const postsNav = document.getElementById("postsNav");
+
+  for (const { id, title } of posts) {
+    const postLink = document.createElement("a");
+
+    postLink.className =
+      "block px-4 py-2.5 text-sm text-indigo-900 hover:bg-indigo-300/70 focus:bg-indigo-300/70 focus:outline-0";
+    postLink.href = "#";
+    postLink.textContent = title;
+    postLink.addEventListener("click", async (event) => {
+      event.preventDefault();
+
+      const post = await getPost(id, token);
+
+      renderPost(post);
+
+      const comments = await getComments(post.id, token);
+
+      renderComments(comments);
+    });
+
+    postsNav.appendChild(postLink);
+  }
+}
+
+function renderPost(post) {
+  const postTitle = document.getElementById("postTitle");
+  const postContent = document.getElementById("postContent");
+
+  postTitle.textContent = post.title;
+  postContent.textContent = post.content;
+}
+
+function renderComments(comments) {
+  const commentsList = document.getElementById("commentsList");
+
+  commentsList.innerHTML = "";
+
+  for (const comment of comments) {
+    const commentListItem = document.createElement("li");
+
+    commentListItem.innerHTML = `
+        <h4 class="text-sm font-semibold text-indigo-700">${comment.author}</h4>
+        <p class="mb-2 text-xs text-indigo-500">${dayjs(comment.createdAt).fromNow()}</p>
+        <p class="text-sm text-indigo-900">${comment.content}</p>
+      `;
+
+    commentsList.appendChild(commentListItem);
+  }
+}
