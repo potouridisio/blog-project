19737b8c7a19f2409c7dfbc3290173f75999616a
Ioannis Potouridis <potouridisio@gmail.com>
2025-04-10 16:17:13 +0300

    Update styles

--- src/utils.js
+++ src/utils.js
@@ -12,7 +12,7 @@ export function renderPosts(posts, token) {
     const postLink = document.createElement("a");
 
     postLink.className =
-      "block px-4 py-2.5 text-sm font-semibold text-indigo-900 hover:bg-indigo-300/70 focus:bg-indigo-300/70 focus:outline-0";
+      "block px-4 py-2.5 text-sm font-medium text-indigo-900 hover:bg-indigo-300/70 focus:bg-indigo-300/70 focus:outline-0";
     postLink.href = "#";
     postLink.textContent = title;
     postLink.addEventListener("click", async (event) => {
@@ -48,7 +48,7 @@ function renderComments(comments) {
     const commentListItem = document.createElement("li");
 
     commentListItem.innerHTML = `
-        <h4 class="text-sm font-semibold text-indigo-700">${comment.author}</h4>
+        <h4 class="mb-2 text-sm font-medium text-indigo-700">${comment.author}</h4>
         <p class="mb-2 text-xs text-indigo-500">${dayjs(comment.createdAt).fromNow()}</p>
         <p class="text-sm text-indigo-900">${comment.content}</p>
       `;
