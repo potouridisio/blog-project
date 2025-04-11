85bdc154430441334b3fd59bfc073052c59c5edd
Ioannis Potouridis <potouridisio@gmail.com>
2025-04-11 10:37:23 +0300

    Add error handling for comment form

--- src/posts.js
+++ src/posts.js
@@ -21,6 +21,29 @@ commentForm.addEventListener("submit", async (event) => {
   const postId = event.target.dataset.postId;
   const content = event.target.content.value;
 
+  if (!content) {
+    const errorMessage = document.createElement("p");
+
+    errorMessage.className = "mt-2 text-sm text-red-500";
+    errorMessage.id = "errorMessage";
+    errorMessage.textContent = "Content field is required";
+
+    const existingErrorMessage = document.getElementById("errorMessage");
+
+    if (existingErrorMessage) {
+      existingErrorMessage.remove();
+    }
+
+    event.target.content.insertAdjacentElement("afterend", errorMessage);
+    event.target.content.className = event.target.content.className.replaceAll(
+      "indigo",
+      "red",
+    );
+    event.target.content.focus();
+
+    return;
+  }
+
   await addComment(postId, content, token);
 
   const comments = await getComments(postId, token);
@@ -28,4 +51,13 @@ commentForm.addEventListener("submit", async (event) => {
   renderComments(comments);
 
   event.target.content.value = "";
+
+  const existingErrorMessage = document.getElementById("errorMessage");
+
+  existingErrorMessage.remove();
+
+  event.target.content.className = event.target.content.className.replaceAll(
+    "red",
+    "indigo",
+  );
 });
